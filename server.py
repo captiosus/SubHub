c0858264285f6006c316f348a623eb3004184ff2
Nydroam <rayw.u17@gmail.com>
2015-05-23 19:57:42 -0400

    python working with objects

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template_string, render_template, request, redirect
 app = Flask(__name__)
 
-
-
-
-
-
+class Sandwich(object):
+    def __init__(self, name = "", creator = "", ingredients = [], rating = 0):
+        self.name = name
+        self.creator = creator
+        self.ingredients = ingredients
+        self.rating = rating
 
 @app.route('/')
 def Home():
@@ -14,9 +15,6 @@ def Home():
 @app.route('/Create', methods=['POST', 'GET'])
 def create():
     return render_template('Create/index.html')
-    if request.method == 'POST':
-        Sandwich = request.form['sandwich']
-        Ingredients = request.form['ingredients']
 
 @app.route('/About')
 def about():
@@ -24,23 +22,29 @@ def about():
 
 @app.route('/Gallery')
 def gallery():
-    f = open("SandwichList.txt", 'r')
-    SList = f.readlines();
-    f.close();
     g = ""
     g += render_template('Gallery/headers.html')
     s = ""
+    Sandwiches = []
+    f = open("SandwichList.txt", 'r')
+    SList = f.readlines()
+    f.close()
     for x in range(0,len(SList)):
-        separate = SList[x].split(':')
+        sep = SList[x].split('::')
+        Sandwiches.append(Sandwich(sep[0],sep[1],sep[2].split(','),sep[3]))
+    for x in range(0,len(Sandwiches)):
         s +='        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">\n'
         s +='          <div class="sandwich">\n'
         s +='            <h2 class="sandwich-name">'
-        s +=separate[0]
+        s +=Sandwiches[x].name
         s +='</h2>\n'
         s +='            <div class="sandwich-picture"></div>\n'
         s +='            <h2 class="ingredients">Ingredients</h2>\n'
         s +='            <p class="ingredients-list">'
-        s +=separate[1]
+        for y in range(0,len(Sandwiches[x].ingredients)):
+            s+=Sandwiches[x].ingredients[y]
+            if y != len(Sandwiches[x].ingredients)-1:
+                s+=", "
         s +='</p>\n'
         s +='          </div>\n'
         s +='        </div>\n'
