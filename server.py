ac8411675f5f70d770062c7fb6e2a5d45594a7ef
Nydroam <rayw.u17@gmail.com>
2015-05-23 14:02:47 -0400

    python flask

--- server.py
+++ server.py
@@ -2,12 +2,20 @@ from flask import Flask, render_template, request, rediret
 app = Flask(__name__)
 
 @app.route('/')
-def Main():
-    return 'Main Page'
+def Home():
+    return 'Home Page'
 
-@app.route('Another Page')
+@app.route('/Create')
 def hello():
-    return 'Hello World'
+    return '...'
+
+@app.route('/Explore')
+def explore():
+    return '...'
+
+@app.route('/About')
+def about():
+    return '...'
 
 if __name__ == '__main__':
     app.run(debug=True)
