c597a848b3a1c64d4989d42d567e5431c4b03c51
Nydroam <rayw.u17@gmail.com>
2015-05-23 23:56:41 -0400

    python sorts done

--- server.py
+++ server.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template_string, render_template, request, redir
 app = Flask(__name__)
 
 class Sandwich(object):
-    def __init__(self, name = "", creator = "", ingredients = [], rating = 0):
+    def __init__(self, name = "", creator = "", Type = "", ingredients = [], likes = 0, dislikes = 0):
         self.name = name
         self.creator = creator
+        self.Type = Type
         self.ingredients = ingredients
-        self.rating = rating
+        self.likes = likes
+        self.dislikes = dislikes
 
 Sandwiches = []
 f = open("SandwichList.txt", 'r')
@@ -14,21 +16,21 @@ SList = f.readlines()
 f.close()
 for x in range(0,len(SList)):
     sep = SList[x].split('::')
-    Sandwiches.append(Sandwich(sep[0],sep[1],sep[2].split(','),sep[3]))
+    Sandwiches.append(Sandwich(sep[0],sep[1],sep[2],sep[3].split(','),sep[4],sep[5]))
 
 def swap(a,b,l):
     c = l[a]
     l[a] = l[b]
     l[b] = c
 
-def partition(a,start,end,param):
-    pivot = a[start].param
+def partition(a,start,end):
     s = start
     e = end
+    pivot = int(a[start].likes)-int(a[start].dislikes)
     while s < e:
-        while a[s].param<pivot:
+        while int(a[s].likes)-int(a[s].dislikes)<pivot:
             s+=1
-        while a[e].param>pivot:
+        while int(a[e].likes)-int(a[e].dislikes)>pivot:
             e-=1
         if s < e:
             swap(s,e,a)
@@ -36,12 +38,52 @@ def partition(a,start,end,param):
             e-=1
     return e
 
-def quickSort( l, start, end, param ):
+def quickSort( l, start, end):
     if start < end:
-        n = partition(l,start,end,param)
-        quickSort(l,start,n,param)
-        quickSort(l,n+1,end,param)
+        n = partition(l,start,end)
+        quickSort(l,start,n)
+        quickSort(l,n+1,end)
+        
+def sortByNewest():
+    f = open("SandwichList.txt",'r')
+    SList = f.readlines()
+    f.close()
+    for x in range(0,len(SList)):
+        sep = SList[x].split('::')
+        Sandwiches[x]=Sandwich(sep[0],sep[1],sep[2],sep[3].split(','),sep[4],sep[5])
 
+def sortByDislikes():
+    sortByNewest()
+    quickSort(Sandwiches,0,len(Sandwiches)-1)
+
+def sortByLikes():
+    sortByNewest()
+    sortByDislikes()
+    Sandwiches.reverse()
+
+def sortByType(param):
+    sortByNewest()
+    i = 0
+    while i < len(Sandwiches):
+        if Sandwiches[i].Type==param:
+            Sandwiches.insert(0,Sandwiches.pop(i))
+        i+=1
+    
+sortByType("Sandwich")
+for x in range(0,len(Sandwiches)):
+    if Sandwiches[x].Type=="Sandwich":
+        print Sandwiches[x].name
+sortByType("Classic")
+for x in range(0,len(Sandwiches)):
+    if Sandwiches[x].Type=="Classic":
+        print Sandwiches[x].name
+sortByNewest()
+for x in range(0,len(Sandwiches)):
+    print Sandwiches[x].name
+sortByLikes()
+for x in range(0,len(Sandwiches)):
+    print Sandwiches[x].name
+    
 def nameCheck(n):
     for x in range(0,len(Sandwiches)):
         if Sandwiches[x].name==n:
@@ -90,7 +132,7 @@ def gallery():
     f.close()
     for x in range(0,len(SList)):
         sep = SList[x].split('::')
-        Sandwiches.append(Sandwich(sep[0],sep[1],sep[2].split(','),sep[3]))
+        Sandwiches.append(Sandwich(sep[0],sep[1],sep[2],sep[3].split(','),sep[4],sep[5]))
     for x in range(0,len(Sandwiches)):
         s +='        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">\n'
         s +='          <div class="sandwich">\n'
@@ -101,6 +143,8 @@ def gallery():
         s +=Sandwiches[x].creator
         s +='</h2>\n'
         s +='            <div class="sandwich-picture"></div>\n'
+        for y in range(0,len(Sandwiches[x].ingredients)):
+            s+='<div class="' + Sandwiches[x].ingredients[y] + '"></div>\n'
         s +='            <h2 class="ingredients">Ingredients</h2>\n'
         s +='            <p class="ingredients-list">'
         for y in range(0,len(Sandwiches[x].ingredients)):
