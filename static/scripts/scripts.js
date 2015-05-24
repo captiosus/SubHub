3b5f3ccb2d7f652699e0c79960015a8e5e6d1dab
captiosus <royxu652@gmail.com>
2015-05-24 03:06:07 -0400

    changes

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -108,25 +108,114 @@ function next(current) {
       if (index == 0) {
         $(this).attr("id", "meats");
         $(this).children("p").html("Meats");
+        $(this).append("<div class='expand-button'>+</div>");
+        $(this).show();
       }
       else if(index == 1) {
+        $(this).addClass("subset");
         $(this).attr("id", "bacon");
         $(this).children("p").html("Bacon");
+        $(this).hide();
       }
       else if(index == 2) {
-        $(this).attr("id", "burger");
-        $(this).children("p").html("Hamburger");
+        $(this).addClass("subset");
+        $(this).attr("id", "chicken");
+        $(this).children("p").html("Chicken");
+        $(this).hide();
       }
       else if(index == 3) {
-        $(this).attr("id", "bagel");
-        $(this).children("p").html("Bagel");
-        $(this).css("display", "block");
+        $(this).addClass("subset");
+        $(this).attr("id", "ham");
+        $(this).children("p").html("Ham");
+        $(this).hide();
       }
       else if(index == 4) {
-        $(this).css("display", "block");
+        $(this).addClass("subset");
+        $(this).attr("id", "salami");
+        $(this).children("p").html("Salami");
+        $(this).hide();
       }
-      if (index > 4) {
-        $(this).css("display", "block");
+      else if(index == 5) {
+        $(this).addClass("subset");
+        $(this).attr("id", "turkey");
+        $(this).children("p").html("Turkey");
+        $(this).hide();
+      }
+      else if(index == 6) {
+        $(this).attr("id", "cheese");
+        $(this).children("p").html("Cheeses");
+        $(this).show();
+      }
+      else if(index == 7) {
+        $(this).addClass("subset");
+        $(this).attr("id", "american");
+        $(this).children("p").html("American");
+        $(this).hide();
+      }
+      else if(index == 8) {
+        $(this).addClass("subset");
+        $(this).attr("id", "swiss");
+        $(this).children("p").html("Swiss");
+        $(this).hide();
+      }
+      else if(index == 9) {
+        $(this).addClass("subset");
+        $(this).attr("id", "mozzarella");
+        $(this).children("p").html("Mozzarella");
+        $(this).hide();
+      }
+      else if(index == 10) {
+        $(this).attr("id", "toppings");
+        $(this).children("p").html("Toppings");
+        $(this).append("<div class='expand-button'></div>");
+        $(this).show();
+      }
+      else if(index == 11) {
+        $(this).addClass("subset");
+        $(this).attr("id", "lettuce");
+        $(this).children("p").html("Lettuce");
+        $(this).hide();
+      }
+      else if(index == 12) {
+        $(this).addClass("subset");
+        $(this).attr("id", "tomato");
+        $(this).children("p").html("Tomato");
+        $(this).hide();
+      }
+      else if(index == 13) {
+        $(this).addClass("subset");
+        $(this).attr("id", "avocado");
+        $(this).children("p").html("Avocado");
+        $(this).hide();
+      }
+      else if(index == 14) {
+        $(this).addClass("subset");
+        $(this).attr("id", "pickles");
+        $(this).children("p").html("Pickles");
+        $(this).hide();
+      }
+      else if(index == 15) {
+        $(this).attr("id", "sauces");
+        $(this).children("p").html("Sauces");
+        $(this).show();
+      }
+      else if(index == 16) {
+        $(this).addClass("subset");
+        $(this).attr("id", "chipotle");
+        $(this).children("p").html("Chipotle");
+        $(this).hide();
+      }
+      else if(index == 17) {
+        $(this).addClass("subset");
+        $(this).attr("id", "mayo");
+        $(this).children("p").html("Mayo");
+        $(this).hide();
+      }
+      else if(index == 18) {
+        $(this).addClass("subset");
+        $(this).attr("id", "ketchup");
+        $(this).children("p").html("Ketchup");
+        $(this).hide();
       }
     });    
   }
